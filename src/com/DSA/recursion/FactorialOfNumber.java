f7b5febb04358c2f27e0d7d9aba3de878a9b48f1
lakshya <lakshyabhasin.cse.20011106@gehu.ac.in>
2022-02-23 19:55:45 +0530

    "added Recursive digits extraction"

--- src/com/DSA/recursion/FactorialOfNumber.java
+++ src/com/DSA/recursion/FactorialOfNumber.java
@@ -1,4 +1,12 @@
 package com.DSA.recursion;
 
 public class FactorialOfNumber {
+        public static void main(String[] args) {
+            System.out.println(factorial(17));
+    }
+    static long factorial(int n){
+        if(n==1)return 1;
+
+        return (n*factorial(n-1));
+    }
 }
