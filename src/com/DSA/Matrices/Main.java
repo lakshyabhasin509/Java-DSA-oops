94a05f0379bf03fb68d3c155854d8895d1b8e9e5
lakshya <lakshyabhasin.cse.20011106@gehu.ac.in>
2022-03-12 11:17:44 +0530

    added QUEUE Examples

--- src/com/DSA/Matrices/Main.java
+++ src/com/DSA/Matrices/Main.java
@@ -1,14 +1,15 @@
 package com.DSA.Matrices;
 
-public class Main {
+public class Mainn {
     public static void main(String[] args) {
-        LowerTriangularMatrix m1=new LowerTriangularMatrix(3);
+        SymmetricMatrix m1=new SymmetricMatrix(3);
         m1.set(1,1,17);
         m1.set(1,2,10);
         m1.set(1,3,1);
         m1.set(2,2,51);
         m1.set(2,3,12);
-        m1.set(3,3,32);
+        m1.set(3,3,32)
+        ;
 
         m1.display();
     }
---
+++ src/com/DSA/Queue/CustomQueue.java
@@ -0,0 +1,4 @@
+package com.DSA.Queue;
+
+public class CustomQueue {
+}
