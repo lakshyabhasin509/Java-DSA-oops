4db43496fee5f6613150109b771de296487fec89
lakshya <lakshyabhasin.cse.20011106@gehu.ac.in>
2022-04-10 16:18:08 +0530

    lambda 2.0

--- src/com/AdvanedJava/Generics/lambaexpression.java
+++ src/com/AdvanedJava/Generics/lambaexpression.java
@@ -1,15 +1,12 @@
 package com.AdvanedJava.Generics;
 @FunctionalInterface
 interface Mylambda{
-    public int display(String str);
+    public int display(int a,int b);
 }
 public class lambaexpression {
     public static void main(String[] args) {
-Mylambda ob=(str)->{
-    System.out.println(str);
-};
-ob.display("less goooo \n"  +
-        "why you still here brahh \n");
+Mylambda ob=(a,b)->a+b;
+        System.out.println(ob.display(10,20));
 
     }
 }
